940bdde50abee3abdd5c08f52db874048f2a01a9
rochac2lee <cleberli.rocha@gmail.com>
2022-02-03 21:49:39 -0300

    Formatando resposta da api pro usuario final

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -17,13 +17,30 @@ class GeoController extends Controller
         $ipClient = "177.36.251.14";
 
         $geoData = $FindIp->getInfoIp($ipClient);
-        $WeaterLocalData = $Weather->getWeatherIp($ipClient);
 
-        $totalInfo = [];
-        array_push($totalInfo, $geoData, $WeaterLocalData);
+        $WeaterLocalData = $Weather->getWeatherCoordinates($geoData->lat, $geoData->lon);
 
-        return response()->json($totalInfo);
+        return response()->json($this->formatResponse($geoData, $WeaterLocalData));
     }
 
+    public function formatResponse($geoData, $WeaterLocalData){
+        
+        $reponseFormatted = [];
+        $atualizacao = explode(" ", $WeaterLocalData->current->last_updated);
+
+        $reponseFormatted['cidade'] = $geoData->city;
+        $reponseFormatted['estado'] = $geoData->regionName;
+        $reponseFormatted['pais'] = $geoData->country;
+
+        $reponseFormatted["temperatura"] = $WeaterLocalData->current->temp_c . "º C";
+        $reponseFormatted["eDia"] = (($WeaterLocalData->current->is_day == 0) ? 'Sim' : 'Nao');
+        $reponseFormatted["status"] = $WeaterLocalData->current->condition->text;
+        $reponseFormatted["velocidadeVento"] = $WeaterLocalData->current->wind_kph . "Km/h";
+        $reponseFormatted["humidadeAr"] = $WeaterLocalData->current->humidity . " %";
+        $reponseFormatted["ultimaAtualizacao"] = $atualizacao[1];
+        
+
+        return $reponseFormatted;
+    }
     
 }
--- app/Services/Weather.php
+++ app/Services/Weather.php
@@ -36,4 +36,30 @@ class Weather{
         
     }
 
+    public function getWeatherCoordinates($latitude, $longitude){
+
+        $url = "$this->endPoint/current.json?key=$this->apiKey&q=$latitude,$longitude&lang=pt";
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($response);
+
+        return $response;
+        
+    }
+
 }
